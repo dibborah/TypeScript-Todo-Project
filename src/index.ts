b63e36d28f4cbc796b6c246d4f24dcd5689e7c4d
dibborah <dibyajyotiborah469@gmail.com>
2024-02-09 19:02:56 +0530

    Bug resolved. Since npm start script was configured in tsconfig and I was trying to run the file through live server the error was there .File won't run from Public folder path since dist folder is not explicitly setup. LocalHost server executes the file

--- src/index.ts
+++ src/index.ts
@@ -12,30 +12,25 @@ const form = document.getElementById("new-task-form") as HTMLElement | null;
 const input = document.querySelector<HTMLInputElement>("#new-task-title");
 
 form?.addEventListener("submit", (e) => {
-  console.log("Before submitting");
-  
   e.preventDefault();
-  
-  console.log("After submitting");
     if(input?.value == "" || input?.value == null) return;
     
     const newTask = {
         id: uuidV4(),
         title: input.value,
-        completed:false,
+        completed: false,
         createdAt : new Date()
     }
-
     addListItem(newTask);
+    input.value = "";
 });
 
 function addListItem(task: Task){
-  console.log("addListItem is called");
-  
   const item = document.createElement("li");
   const label = document.createElement("label");
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
+  checkbox.checked = true;
   label.append(checkbox, task.title);
   item.append(label);
   list?.append(item);
